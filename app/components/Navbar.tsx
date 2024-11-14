89c1d40fd188600af8a19aae0528e59970edaac9
Dmitrii <dimterion59@gmail.com>
2024-11-14 11:45:23 +0100

    adjust navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,3 +1,14 @@
+import Image from "next/image";
+import Link from "next/link";
+
 export default function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <div className="px-5 py-3 bg-white shadow-sm font-work-sans">
+      <nav className="flex justify-between items-center">
+        <Link href="/">
+          <Image src="/logo.png" alt="Logo" width={144} height={30} />
+        </Link>
+      </nav>
+    </div>
+  );
 }
