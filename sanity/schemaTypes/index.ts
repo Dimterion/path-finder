1622aae2061192c983705d240b95be652d4755e3
Dmitrii <dimterion59@gmail.com>
2024-11-20 11:46:31 +0100

    add sanity md & types

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,5 +1,7 @@
 import { type SchemaTypeDefinition } from "sanity";
+import { author } from "@/sanity/schemaTypes/author";
+import { path } from "@/sanity/schemaTypes/path";
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [],
+  types: [author, path],
 };
