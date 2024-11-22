b23c9091cba8b981d2849938cad48c3f6d644ed6
Dmitrii <dimterion59@gmail.com>
2024-11-22 11:34:01 +0100

    add path page elements

--- app/(root)/path/[id]/page.tsx
+++ app/(root)/path/[id]/page.tsx
@@ -1,5 +1,8 @@
+import Link from "next/link";
+import Image from "next/image";
 import { client } from "@/sanity/lib/client";
 import { PATH_BY_ID_QUERY } from "@/sanity/lib/queries";
+import { formatDate } from "@/lib/utils";
 import { notFound } from "next/navigation";
 
 export const experimental_ppr = true;
@@ -17,7 +20,44 @@ export default async function Path({
 
   return (
     <>
-      <h1 className="text-3xl">{path.title}</h1>
+      <section className="pink_container !min-h-[230px]">
+        <p className="tag">{formatDate(path?._createdAt)}</p>
+        <h1 className="heading">{path.title}</h1>
+        <p className="sub-heading !max-w-5xl">{path.description}</p>
+      </section>
+      <section className="section_container">
+        <Image
+          src={path.image}
+          alt="Thumbnail"
+          width={164}
+          height={164}
+          className="m-auto h-auto max-h-[250px] w-full max-w-[250px] rounded-xl"
+        />
+        <div className="mx-auto mt-10 max-w-4xl space-y-5">
+          <div className="flex-between gap-5">
+            <Link
+              href={`/user/${path.authro?._id}`}
+              className="mb-3 flex gap-2"
+            >
+              <Image
+                src={path.author.image}
+                alt="Avatar"
+                width={64}
+                height={64}
+                className="rounded-full drop-shadow-lg"
+              />
+              <div>
+                <p className="text-20-medium">{path.author.name}</p>
+                <p className="text-16-medium !text-black-300">
+                  @{path.author.username}
+                </p>
+              </div>
+            </Link>
+            <p className="category-tag">{path.category}</p>
+          </div>
+          <h3 className="text-30-bold">Pitch Details</h3>
+        </div>
+      </section>
     </>
   );
 }
