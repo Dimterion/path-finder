85afa7ebc2f28f3e92c0502684858fd0718189fc
Dmitrii <dimterion59@gmail.com>
2024-11-22 15:08:37 +0100

    add write-client

--- components/View.tsx
+++ components/View.tsx
@@ -1,11 +1,21 @@
+import { unstable_after as after } from "next/server";
 import { client } from "@/sanity/lib/client";
 import { PATH_VIEWS_QUERY } from "@/sanity/lib/queries";
+import { writeClient } from "@/sanity/lib/write-client";
 import Ping from "./Ping";
 
 export default async function View({ id }: { id: string }) {
   const { views: totalViews } = await client
     .withConfig({ useCdn: false })
     .fetch(PATH_VIEWS_QUERY, { id });
+
+  after(
+    async () =>
+      await writeClient
+        .patch(id)
+        .set({ views: totalViews + 1 })
+        .commit(),
+  );
 
   return (
     <div className="view-container">
