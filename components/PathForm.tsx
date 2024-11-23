8e7e77516f2d33a21965f71b2b7db1a0114dc407
Dmitrii <dimterion59@gmail.com>
2024-11-23 12:22:39 +0100

    add form components

--- components/PathForm.tsx
+++ components/PathForm.tsx
@@ -1,3 +1,22 @@
+"use client";
+
+import { Input } from "@/components/ui/input";
+
 export default function PathForm() {
-  return <>Path Form</>;
+  return (
+    <form action={() => {}} className="path-form">
+      <div>
+        <label htmlFor="title" className="path-form_label">
+          Title
+        </label>
+        <Input
+          id="title"
+          name="title"
+          className="path-form_input"
+          required
+          placeholder="Path title"
+        />
+      </div>
+    </form>
+  );
 }
