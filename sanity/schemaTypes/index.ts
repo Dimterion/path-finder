c4d7158707067dff7622b396a8097904b3ff5559
Dmitrii <dimterion59@gmail.com>
2024-11-26 10:53:03 +0100

    add playlist schemaType

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,7 +1,8 @@
 import { type SchemaTypeDefinition } from "sanity";
 import { author } from "@/sanity/schemaTypes/author";
 import { path } from "@/sanity/schemaTypes/path";
+import { playlist } from "./playlist";
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [author, path],
+  types: [author, path, playlist],
 };
--- sanity/types.ts
+++ sanity/types.ts
@@ -125,6 +125,23 @@ export type SanityAssetSourceData = {
   url?: string;
 };
 
+export type Playlist = {
+  _id: string;
+  _type: "playlist";
+  _createdAt: string;
+  _updatedAt: string;
+  _rev: string;
+  title?: string;
+  slug?: Slug;
+  select?: Array<{
+    _ref: string;
+    _type: "reference";
+    _weak?: boolean;
+    _key: string;
+    [internalGroqTypeReferenceTo]?: "path";
+  }>;
+};
+
 export type Path = {
   _id: string;
   _type: "path";
@@ -179,6 +196,7 @@ export type AllSanitySchemaTypes =
   | SanityImageMetadata
   | Geopoint
   | SanityAssetSourceData
+  | Playlist
   | Path
   | Slug
   | Author
