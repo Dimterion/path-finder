48e75863e22ae94fcd54e1775249befc4b89bd48
Dmitrii <dimterion59@gmail.com>
2026-09-13 21:33:42 +0200

    add base listings screen & data

---
+++ app/listings.tsx
@@ -0,0 +1,50 @@
+import { listings } from "../features/listings/data";
+import { FlatList, StyleSheet, Text, View } from "react-native";
+import PlatformCard from "../features/platforms/PlatformCard";
+
+export default function ListingsScreen() {
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={listings}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => <PlatformCard item={item} />}
+        contentContainerStyle={styles.listContent}
+        ListHeaderComponent={
+          <View style={styles.header}>
+            <Text style={styles.screenTitle}>Job Listings</Text>
+            <Text style={styles.screenText}>
+              Explore different job search listings and learn what each one can
+              offer.
+            </Text>
+          </View>
+        }
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#f5f7fb",
+  },
+  listContent: {
+    padding: 16,
+    paddingBottom: 32,
+  },
+  header: {
+    marginBottom: 20,
+  },
+  screenTitle: {
+    fontSize: 28,
+    fontWeight: "700",
+    marginBottom: 8,
+    color: "#111827",
+  },
+  screenText: {
+    fontSize: 16,
+    lineHeight: 24,
+    color: "#4b5563",
+  },
+});
